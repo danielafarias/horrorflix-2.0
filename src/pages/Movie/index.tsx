a12848bf4eef56e56d0b35e7c48988f4eeb1916e
Daniela Farias <farias.danicris@gmail.com>
2023-03-11 17:19:07 -0300

    feat: movie page

--- src/pages/Movie/index.tsx
+++ src/pages/Movie/index.tsx
@@ -1,6 +1,107 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import Loader from "../../components/Loader";
+import api from "../../services/api";
+import { Container } from "./styles";
+
+interface MovieType {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: null;
+  budget: number;
+  genres: {
+    id: number;
+    name: string;
+  }[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: {
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
 export default function Movie() {
-    return (
-        <>
-        </>
-    );
+  const { id } = useParams();
+
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState<MovieType>({} as MovieType);
+
+  useEffect(() => {
+    handleMovie();
+  }, []);
+
+  async function handleMovie() {
+    try {
+      const response = await api.get(`movie/${id}`, {
+        params: {
+          api_key: "3c00eaa45868cd68c6653fa2a92cdbf6",
+          language: "pt-BR",
+        },
+      });
+
+      setMovie(response.data);
+      setLoading(false);
+    } catch {
+      setLoading(false);
+    }
+  }
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <Container
+      backgroundImg={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+    >
+      <div className="overlay">
+        <div className="movie-info">
+          <h1>{movie.title}</h1>
+          <article className="movie-sinopse">
+            <h3>Sinopse</h3>
+            <p>{movie.overview}</p>
+          </article>
+          <strong>
+            Avaliação: <span>{movie.vote_average} / 10</span>
+          </strong>
+          <div className="movie-buttons">
+            <button className="save-btn">Salvar</button>
+            <Link className="trailer-btn" to="#">Trailer</Link>
+          </div>
+        </div>
+        <img
+          src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+          alt={movie.title}
+        />
+      </div>
+    </Container>
+  );
 }
---
+++ src/pages/Movie/styles.ts
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {
+  backgroundImg: string;
+}
+
+export const Container = styled.div<ContainerProps>`
+  background-image: url(${(props) => props.backgroundImg});
+  background-position: center;
+  background-repeat: no-repeat;
+
+  .overlay {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.8);
+
+    .movie-info {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      padding: 2rem;
+      height: 80vh;
+
+      .movie-sinopse {
+        h3 {
+          margin-bottom: 1rem;
+        }
+      }
+
+      .movie-buttons {
+        display: flex;
+        .trailer-btn {
+          border: 2px solid var(--text2);
+          background-color: var(--text2);
+
+          &:hover {
+            color: var(--text2);
+          }
+        }
+
+        .save-btn {
+          border: 2px solid var(--primary);
+          background-color: var(--primary);
+
+          &:hover {
+            color: var(--primary);
+          }
+        }
+
+        .save-btn,
+        .trailer-btn {
+          margin-right: 1rem;
+          display: flex;
+          align-items: center;
+          padding: 0.5rem;
+          font-weight: bold;
+          color: var(--secondary);
+          border-radius: 5px;
+          cursor: pointer;
+          justify-content: center;
+          font-size: 1.5rem;
+          max-width: 6rem;
+
+          &:hover {
+            background-color: transparent;
+          }
+        }
+      }
+    }
+  }
+
+  img {
+    width: 30rem;
+  }
+
+  @media screen and (max-width: 35em) {
+    img {
+      display: none;
+    }
+  }
+`;
