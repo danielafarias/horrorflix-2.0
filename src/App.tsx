d39f139286607677d5264f388b6b1bafda17a7f4
Daniela Farias <farias.danicris@gmail.com>
2023-03-12 14:48:06 -0300

    clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import './App.css';
-import { RoutesApp } from './routes';
-import GlobalStyles from './styles/globalStyles';
-import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
+import { RoutesApp } from "./routes";
+
+import GlobalStyles from "./styles/globalStyles";
 
 function App() {
   return (
     <>
-      <ToastContainer autoClose={3000} theme={'dark'} />
+      <ToastContainer autoClose={3000} theme={"dark"} />
       <RoutesApp />
       <GlobalStyles />
     </>
